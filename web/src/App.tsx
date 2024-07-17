552a21d9060abc7aedd54316d73ffab1cc62bced
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-07-17 15:17:31 -0300

    chore: install and config shadcnui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
   return (
     <section>
       <h1 className="text-3xl font-bold underline">Home</h1>
+      <Button>tetse</Button>
     </section>
   );
 }
